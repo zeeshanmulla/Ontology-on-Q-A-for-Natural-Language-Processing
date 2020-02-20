a7adfd69e5751ee61f72dceb5b0f0f93beba2e84
Mohammed Zeeshan Mulla <zeeshanmullamail@gmail.com>
2020-02-20 15:16:20 +0530

    dict

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+/*
+SQLyog Community Edition- MySQL GUI v7.01 
+MySQL - 5.0.27-community-nt : Database - qa_ontology
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
